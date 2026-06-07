34c5928e0069f9d61a70e87f5d5252f41f49687e
Rushikamgr9 <rushikamagar9@gmail.com>
2026-06-07 21:32:47 +0545

    feat: taskroutes.js file created

---
+++ routes/taskroutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+const taskController =
+require("../controllers/taskController");
+
+router.get(
+    "/",
+    taskController.getAllTasks
+);
+
+router.post(
+    "/",
+    taskController.createTask
+);
+
+router.get(
+    "/:id",
+    taskController.getTaskById
+);
+
+router.delete(
+    "/:id",
+    taskController.deleteTask
+);
+
+module.exports = router;
